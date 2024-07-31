a26af8a11144180c1ec525cd80147cb728428873
bsk2002 <69530570+bsk2002@users.noreply.github.com>
2024-07-31 11:37:52 +0900

    add colorArr for timetable

--- agthon_2/script.js
+++ agthon_2/script.js
@@ -1,9 +1,14 @@
+let colorArr = ["#ffadad", "#ffd6a5", "#fdffb6", "#caffbf", "#9bf6ff", "#a0c4ff", "#bdb2ff", "#ffc6ff"];
+let count = 0;
+
 function changeColor() {
     let p = document.getElementsByTagName("th");
-    p[12].style.backgroundColor = "blue";
-    p[18].style.backgroundColor = "blue";
-    p[24].style.backgroundColor = "blue";
+    p[12].style.backgroundColor = colorArr[count];
+    p[18].style.backgroundColor = colorArr[count];
+    p[24].style.backgroundColor = colorArr[count];
+    count++;
 }
+
 function reset() {
     let p = document.getElementsByTagName("th");
     p[12].style.backgroundColor = "white";
