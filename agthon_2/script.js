2b6ca915bbda3eff55799439bb0362aaa723a3a3
bsk2002 <69530570+bsk2002@users.noreply.github.com>
2024-07-31 17:57:25 +0900

    save 0730

--- agthon_2/script.js
+++ agthon_2/script.js
@@ -6,8 +6,7 @@ function changeColor() {
     let tmpSubjectArrValue = JSON.parse(localStorage.getItem("tmpSubjectArrValue")) || [];
 
     for (let i = 0; i < tmpSubjectArrValue.length; i++) {
-        p[tmpSubjectArrValue[i].value].style.backgroundColor = colorArr[count % colorArr.length];
-        count++;
+        p[tmpSubjectArrValue[i].value].style.backgroundColor = tmpSubjectArrValue[i].color;
     }
 }
 
@@ -23,14 +22,15 @@ function reset() {
 
 function tmpSubject(e) {
     let tmpSubjectArrValue = JSON.parse(localStorage.getItem("tmpSubjectArrValue")) || [];
-    let subjectName = e.previousSibling.textContent.trim();
+    let subjectName = e.parentNode.previousElementSibling.innerText;
     let value = parseInt(e.value);
     let timeClass = e.className;
-    let subject = { name: subjectName, value: value, time: timeClass };
+    let subject = { name: subjectName, value: value, time: timeClass, color: colorArr[count] };
 
     // 값이 배열에 포함되어 있는지 확인
     if (!tmpSubjectArrValue.some(item => item.value === value)) {
         tmpSubjectArrValue.push(subject);
+        count++;
         localStorage.setItem("tmpSubjectArrValue", JSON.stringify(tmpSubjectArrValue));
         alert("장바구니에 정상적으로 추가되었습니다. 확인을 위해 '장바구니 새로고침' 버튼을 눌러주세요");
         updateCartTable(); // 실시간 업데이트 호출
@@ -63,5 +63,7 @@ function displayCartSubjects() {
         row.appendChild(nameCell);
         row.appendChild(timeCell);
         cartTableBody.appendChild(row);
+
+        row.style.backgroundColor = subject.color;
     });
 }
