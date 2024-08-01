3be1d7f467b1c80fb82327702a0c0ba704682297
bsk2002 <69530570+bsk2002@users.noreply.github.com>
2024-08-01 19:04:46 +0900

    시간표 기능 최종

--- agthon_2/script.js
+++ agthon_2/script.js
@@ -2,16 +2,17 @@ let colorArr = ["#ffadad", "#ffd6a5", "#fdffb6", "#caffbf", "#9bf6ff", "#a0c4ff"
 let count = 0;
 
 function changeColor() {
-    let p = document.getElementsByTagName("th");
+    let p = document.getElementById("mytable").getElementsByTagName("td");
     let tmpSubjectArrValue = JSON.parse(localStorage.getItem("tmpSubjectArrValue")) || [];
-
     for (let i = 0; i < tmpSubjectArrValue.length; i++) {
-        p[tmpSubjectArrValue[i].value].style.backgroundColor = tmpSubjectArrValue[i].color;
+        for (let j = 0; j < tmpSubjectArrValue[i].value.length; j++) {
+            p[tmpSubjectArrValue[i].value[j]].style.backgroundColor = tmpSubjectArrValue[i].color;
+        }
     }
 }
 
 function reset() {
-    let p = document.getElementsByTagName("th");
+    let p = document.getElementById("mytable").getElementsByTagName("td");
     for (let i = 0; i < p.length; i++) {
         p[i].style.backgroundColor = "";
     }
@@ -20,20 +21,55 @@ function reset() {
 
 }
 
+function resetThis(e) {
+    let p = document.getElementById("mytable").getElementsByTagName("td");
+
+    let tmpSubjectArrValue = JSON.parse(localStorage.getItem("tmpSubjectArrValue")) || [];
+    let tmpElement = e.parentNode.parentNode;
+    let nameThis = tmpElement.firstElementChild.innerText;
+    let index = 0;
+    while (true) {
+        if (tmpSubjectArrValue[index].name === nameThis) {
+            break;
+        }
+        index++;
+    }
+    alert(tmpSubjectArrValue[index].value)
+    for (let i = 0; i < tmpSubjectArrValue[index].value.length; i++) {
+        p[tmpSubjectArrValue[index].value[i]].style.backgroundColor = "";
+    }
+
+    let removeThis = e.parentNode.parentNode;
+    let removeParent = removeThis.parentNode;
+    removeParent.removeChild(removeThis);
+    tmpSubjectArrValue.splice(index, 1);
+    localStorage.setItem("tmpSubjectArrValue", JSON.stringify(tmpSubjectArrValue));
+}
+
 function tmpSubject(e) {
     let tmpSubjectArrValue = JSON.parse(localStorage.getItem("tmpSubjectArrValue")) || [];
     let subjectName = e.parentNode.previousElementSibling.innerText;
-    let value = parseInt(e.value);
-    let timeClass = e.className;
-    let subject = { name: subjectName, value: value, time: timeClass, color: colorArr[count] };
+    let times = JSON.parse(e.getAttribute("data-times"))
+    let subject = { name: subjectName, value: times, color: colorArr[count] };
 
     // 값이 배열에 포함되어 있는지 확인
-    if (!tmpSubjectArrValue.some(item => item.value === value)) {
+    let isTimeConflict = tmpSubjectArrValue.some(item => {
+        return item.value.some(time => times.includes(time));
+    });
+
+    let isNameConflict = tmpSubjectArrValue.some(item => {
+        return item.name.includes(subjectName)
+    })
+    if (!isTimeConflict) {
         tmpSubjectArrValue.push(subject);
         count++;
         localStorage.setItem("tmpSubjectArrValue", JSON.stringify(tmpSubjectArrValue));
         alert("장바구니에 정상적으로 추가되었습니다. 확인을 위해 '장바구니 새로고침' 버튼을 눌러주세요");
         updateCartTable(); // 실시간 업데이트 호출
+    } else if (!isNameConflict) {
+        {
+            alert("강의 시간이 겹칩니다.")
+        }
     } else {
         alert("이미 추가된 과목입니다.");
     }
@@ -56,13 +92,16 @@ function displayCartSubjects() {
         let row = document.createElement("tr");
         let nameCell = document.createElement("td");
         let timeCell = document.createElement("td");
+        let buttonCell = document.createElement("td");
 
         nameCell.textContent = subject.name;
-        timeCell.textContent = subject.time + " (" + subject.value + ")";
+        timeCell.textContent = subject.value;
 
         row.appendChild(nameCell);
         row.appendChild(timeCell);
+        row.appendChild(buttonCell);
         cartTableBody.appendChild(row);
+        buttonCell.innerHTML = "<button onclick=" + "resetThis(this);" + ">제거</button>"
 
         row.style.backgroundColor = subject.color;
     });
